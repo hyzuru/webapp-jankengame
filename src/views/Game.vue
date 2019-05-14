4b0a1b81846eb54aa67e8e1a8cd04a1a17c686e3
hyzuru <asatenshi@gmail.com>
2019-05-14 11:16:41 +0900

    create janken game
    created a game view and added janken component, added images

---
+++ src/views/Game.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+<Janken></Janken>
+
+
+</div>
+    
+</template>
+<script>
+import Janken from '@/components/Janken'
+export default {
+    name: 'Game',
+    components:{
+        Janken
+    }
+}
+</script>
